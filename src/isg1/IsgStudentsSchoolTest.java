a37141f05af3b4a45e9c1450361febcc559a7b2d
djeket <ouatt@hotmail.com>
2022-02-16 21:55:13 -0500

    Added new test scenario for student enrollment happy path.

--- src/isg1/IsgStudentsSchoolTest.java
+++ src/isg1/IsgStudentsSchoolTest.java
@@ -33,8 +33,142 @@ public class IsgStudentsSchoolTest extends IsgStudentsSchoolBaseTest {
 		getWebDriver().findElement(By.xpath("/html/body/app-root/div/app-login/div/div/form/button")).click();
 		Thread.sleep(5000);
 	}
-
 	@Test(priority = 2)
+	public void newStudentEnrollement() throws InterruptedException {
+		getWebDriver().get("http://angular-project-isg.s3-website-us-east-1.amazonaws.com");
+		getWebDriver().findElement(By.xpath("/html/body/app-root/div/app-main/div/div/div[2]/div/span[2]/h3/a"))
+				.click();
+		// enter username and password
+		getWebDriver().findElement(By.xpath("//input[@name='username']")).sendKeys("amy");
+		getWebDriver().findElement(By.xpath("//input[@name='password']")).sendKeys("1962");
+
+		// click to sign in
+		getWebDriver().findElement(By.xpath("/html/body/app-root/div/app-login/div/div/form/button")).click();
+		Thread.sleep(5000);
+
+		// students new rnrollement
+		getWebDriver().findElement(By.xpath("/html/body/app-root/div/app-welcome/div/div/div[3]/a")).click();
+		//// Contact Information Form
+		// full name
+		getWebDriver().findElement(By.xpath("/html/body/app-root/div/app-contact/div/div/form/div[1]/div[1]/input"))
+				.sendKeys("Morifere toure");
+		// relationship Student
+		getWebDriver().findElement(By.xpath("/html/body/app-root/div/app-contact/div/div/form/div[1]/div[2]/input"))
+				.sendKeys("Father");
+		// primary phone number
+		getWebDriver().findElement(By.xpath("/html/body/app-root/div/app-contact/div/div/form/div[2]/div[1]/input"))
+				.sendKeys("301-432-0000");
+		// primary email
+		getWebDriver().findElement(By.xpath("/html/body/app-root/div/app-contact/div/div/form/div[2]/div[2]/input"))
+				.sendKeys("Toure@gmail.com");
+		// Alter Phone number
+		getWebDriver().findElement(By.xpath("/html/body/app-root/div/app-contact/div/div/form/div[3]/div[1]/input"))
+				.sendKeys("514-432-0000");
+		// Alt Email
+		getWebDriver().findElement(By.xpath("/html/body/app-root/div/app-contact/div/div/form/div[3]/div[2]/input"))
+				.sendKeys(" Mor@gmail.com");
+
+		// Student1 Information
+
+		// FirstName
+		getWebDriver()
+				.findElement(By.xpath(
+						"/html/body/app-root/div/app-contact/div/div/form/div[4]/div/div[2]/div[1]/div[1]/input"))
+				.sendKeys("Adama");
+		// Midle Name
+		getWebDriver()
+				.findElement(By.xpath(
+						"/html/body/app-root/div/app-contact/div/div/form/div[4]/div/div[2]/div[1]/div[2]/input"))
+				.sendKeys("Moriba");
+		// Last name
+		getWebDriver()
+				.findElement(By.xpath(
+						"/html/body/app-root/div/app-contact/div/div/form/div[4]/div/div[2]/div[1]/div[3]/input"))
+				.sendKeys("Toure");
+		// Gender
+		getWebDriver()
+				.findElement(By.xpath(
+						"/html/body/app-root/div/app-contact/div/div/form/div[4]/div/div[2]/div[2]/div[1]/select"))
+				.sendKeys("Male");
+		// Date of Birth
+		getWebDriver()
+				.findElement(By.xpath(
+						"/html/body/app-root/div/app-contact/div/div/form/div[4]/div/div[2]/div[2]/div[2]/input"))
+				.sendKeys("01/14/2010");
+		// Ages
+
+		getWebDriver()
+				.findElement(By.xpath(
+						"/html/body/app-root/div/app-contact/div/div/form/div[4]/div/div[2]/div[2]/div[3]/input"))
+				.sendKeys("12");
+		// allergies
+		getWebDriver()
+				.findElement(By.xpath(
+						"/html/body/app-root/div/app-contact/div/div/form/div[4]/div/div[2]/div[3]/div/textarea"))
+				.sendKeys("chocolate");
+		// Class Days
+		getWebDriver()
+				.findElement(By.xpath(
+						"/html/body/app-root/div/app-contact/div/div/form/div[4]/div/div[2]/div[4]/div[1]/select"))
+				.sendKeys("Sartuday");
+		// Class Acording To Ages Group
+		getWebDriver()
+				.findElement(By.xpath(
+						"/html/body/app-root/div/app-contact/div/div/form/div[4]/div/div[2]/div[4]/div[2]/select"))
+				.sendKeys("Proficient");
+		// Parent Guardian/Parent Full Name
+
+		getWebDriver().findElement(By.xpath(
+				"/html/body/app-root/div/app-contact/div/div/form/div[4]/div/div[2]/div[5]/div/div/div/div[1]/div/input"))
+				.sendKeys("Yousouf Toure");
+		// Adress one
+		getWebDriver().findElement(By.xpath(
+				"/html/body/app-root/div/app-contact/div/div/form/div[4]/div/div[2]/div[5]/div/div/div/div[2]/div[1]/input"))
+				.sendKeys("1254 Fall Rd");
+		// Addres Two
+		getWebDriver().findElement(By.xpath(
+				"/html/body/app-root/div/app-contact/div/div/form/div[4]/div/div[2]/div[5]/div/div/div/div[2]/div[2]/input"))
+				.sendKeys("215 Elm st");
+		// city
+		getWebDriver().findElement(By.xpath(
+				"/html/body/app-root/div/app-contact/div/div/form/div[4]/div/div[2]/div[5]/div/div/div/div[3]/div[1]/input"))
+				.sendKeys("Boyds");
+		// State
+		getWebDriver().findElement(By.xpath(
+				"/html/body/app-root/div/app-contact/div/div/form/div[4]/div/div[2]/div[5]/div/div/div/div[3]/div[2]/input"))
+				.sendKeys("MD");
+		// Zip code
+		getWebDriver().findElement(By.xpath(
+				"/html/body/app-root/div/app-contact/div/div/form/div[4]/div/div[2]/div[5]/div/div/div/div[3]/div[3]/input"))
+				.sendKeys("20800");
+		// email
+		getWebDriver().findElement(By.xpath(
+				"/html/body/app-root/div/app-contact/div/div/form/div[4]/div/div[2]/div[5]/div/div/div/div[4]/div[1]/input"))
+				.sendKeys("you@gmail.com");
+		// Phone
+		getWebDriver().findElement(By.xpath(
+				"/html/body/app-root/div/app-contact/div/div/form/div[4]/div/div[2]/div[5]/div/div/div/div[4]/div[2]/input"))
+				.sendKeys("632-432-1100");
+		// Alt Email
+		getWebDriver().findElement(By.xpath(
+				"/html/body/app-root/div/app-contact/div/div/form/div[4]/div/div[2]/div[5]/div/div/div/div[5]/div[1]/input"))
+				.sendKeys("tou@gmail.com");
+		// Alt Phone
+		getWebDriver().findElement(By.xpath(
+				"//html/body/app-root/div/app-contact/div/div/form/div[4]/div/div[2]/div[5]/div/div/div/div[5]/div[2]/input"))
+				.sendKeys("240-432-6666");
+		// Relationship To Student
+		getWebDriver().findElement(By.xpath(
+				"/html/body/app-root/div/app-contact/div/div/form/div[4]/div/div[2]/div[5]/div/div/div/div[6]/div/input"))
+				.sendKeys("Uncle");
+		// save form
+		// getWebDriver().findElement(By.xpath("/html/body/app-root/div/app-contact/div/div/form/div[6]/button[1]")).click();
+		 // Clear form
+		getWebDriver().findElement(By.xpath("/html/body/app-root/div/app-contact/div/div/form/div[6]/button[2]")).click();
+		
+	}
+
+	@Test(priority = 3)
 	public void createNewAccount() throws InterruptedException {
 		getWebDriver().get("http://angular-project-isg.s3-website-us-east-1.amazonaws.com");
 		getWebDriver().findElement(By.xpath("/html/body/app-root/div/app-main/div/div/div[2]/div/span[2]/h3/a"))
@@ -51,4 +185,5 @@ public class IsgStudentsSchoolTest extends IsgStudentsSchoolBaseTest {
 		Thread.sleep(2000);
 		// driver.findElement(By.linkText("I have an Account!")).click();
 	}
+
 }
